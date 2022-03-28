7d746f9ddcb1eaba61775bccf84e8fe38367110f
Junior <duartejusanca@gmail.com>
2022-03-28 11:51:38 +0200

    change dotenvexemple file

--- app/Controllers/UserController.js
+++ app/Controllers/UserController.js
@@ -7,19 +7,23 @@ module.exports = class UserController {
         this.req = req
         this.res = res
     }
+    
     async getAll() {
         const users = new UserRepository(this.req, this.res);
         const results = await users.all();
         return results;
     }
+    
     async get(id) {
         const users = new UserRepository(this.req, this.res);
         const results = await users.get(id);
+        
         if (results.length == 0) {
             this.res.writeHead(404, { "Content-type": "text/plain" })
-            this.res.write('user not found with id:' + id);
+            this.res.write('there is no user with this id:' + id);
             this.res.end();
         }
+        
         else {
             this.res.write(JSON.stringify(results));
             this.res.end();
@@ -27,19 +31,23 @@ module.exports = class UserController {
         }
 
     }
+    
     async post(name, email, password) {
         const users = new UserRepository(this.req, this.res);
         const results = await users.create(name, email, password);
         return results;
     }
+
     async update(id, name, email, password) {
         const users = new UserRepository(this.req, this.res);
         const results = await users.update(id, name, email, password);
+        
         if (results.rowCount == 0) {
             this.res.writeHead(404, { "Content-type": "text/plain" })
-            this.res.write('user not found with id:' + id);
+            this.res.write('there is no user with this id:' + id);
             this.res.end();
         }
+        
         else {
             this.res.writeHead(200, { "Content-type": "text/plain" })
             this.res.write('user updated');
@@ -47,14 +55,17 @@ module.exports = class UserController {
             return results;
         }
     }
+
     async delete(id) {
         const users = new UserRepository(this.req, this.res);
         const results = await users.delete(id);
+        
         if (results.rowCount == 0) {
             this.res.writeHead(404, { "Content-type": "text/plain" })
-            this.res.write('user not found with id:' + id);
+            this.res.write('there is no user with this id:' + id);
             this.res.end();
         }
+
         else {
             this.res.writeHead(200, { "Content-type": "text/plain" })
             this.res.write('user deleted');
